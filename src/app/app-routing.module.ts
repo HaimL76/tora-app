c4ec0ab1507d199dc39f5f6cf8bd6f1a9c83de85
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2020-08-11 22:45:35 +0300

    commit 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PersonComponent } from './person/person.component';
+
 const routes: Routes = [];
 
 @NgModule({
