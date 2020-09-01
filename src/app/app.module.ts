72886e22f8b1191cfece991166d599a53fccc859
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2020-09-01 19:55:02 +0300

    24

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +7,9 @@ import { PersonComponent } from './person/person.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { PersonDetailsComponent } from './person-details/person-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,12 @@ import { PersonDetailsComponent } from './person-details/person-details.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatProgressBarModule
+  ],
+  schemas: [ 
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   providers: [],
   bootstrap: [AppComponent]
